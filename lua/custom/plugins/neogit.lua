8d37f43583093d9b194e640c70e5f0a45ed1c931
nom <nom@archy.com>
2026-05-15 13:18:29 +0200

    added neogit

---
+++ lua/custom/plugins/neogit.lua
@@ -0,0 +1,7 @@
+vim.pack.add {
+  { src = 'https://github.com/NeogitOrg/neogit' },
+  { src = 'https://github.com/nvim-lua/plenary.nvim' },
+  { src = 'https://github.com/sindrets/diffview.nvim' },
+  { src = 'https://github.com/nvim-telescope/telescope.nvim' },
+}
+require('neogit').setup {}
