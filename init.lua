e59b392480fa5ef58827f873ec72d8b5d4efbe50
nom <nom@archy.com>
2026-05-15 13:01:34 +0200

    netrw keybind

--- init.lua
+++ init.lua
@@ -177,6 +177,7 @@ do
 
   -- [[ Basic Keymaps ]]
   --  See `:help vim.keymap.set()`
+  vim.keymap.set('n', '<leader>p', vim.cmd.Ex, { desc = 'Open file browser (netrw)' })
 
   -- Clear highlights on search when pressing <Esc> in normal mode
   --  See `:help hlsearch`
